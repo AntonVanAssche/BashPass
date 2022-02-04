ebcf298ecd061abf32d6c6e412cefbec7bc82d64
Anton Van Assche <vanasscheanton@gmail.com>
2022-02-04 14:40:43 +0100

    Add update function

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-Abort the script when an error occures.
+# Abort the script when an error occures.
 set -e
 
 currentDir="$(pwd)"
+
+configLocation="$HOME/.config/bashpass"
+config="$configLocation/bashpass.conf"
+oldVersion="$(grep "version" "$config" | cut -d: -f2)"
+newVersion="$(grep "version" "$currentDir/config/bashpass.conf" | cut -d: -f2)"
 
 # Detect user
 if [[ "$UID" == 0 ]]; then
@@ -11,10 +16,27 @@ if [[ "$UID" == 0 ]]; then
    exit 1
 fi
 
-mkdir -p "$HOME/.config/bashpass"
-mkdir -p "$HOME/.local/share/bashpass"
+function Update() {
+   printf "BashPass is already installed on your system"
+   printf "\nUpdating BashPass..."
 
-cp -r "$currentDir/config/bashpass.conf" "$HOME/.config/bashpass/bashpass.conf"
-cp -r "$currentDir/bashpass" "$HOME/.local/bin/bashpass"
+   sed -i "1s|$oldVersion|$newVersion|g" "$config"
+   cp -r "$currentDir/bashpass" "$HOME/.local/bin/bashpass"
+}
 
-printf "Make sure to add '\$HOME/.local/bin/' to your \$PATH."
+function Install() {
+   printf "Installing BashPass..."
+   mkdir -p "$HOME/.config/bashpass"
+   mkdir -p "$HOME/.local/share/bashpass"
+
+   cp -r "$currentDir/config/bashpass.conf" "$HOME/.config/bashpass/bashpass.conf"
+   cp -r "$currentDir/bashpass" "$HOME/.local/bin/bashpass"
+
+   printf "\nMake sure to add '\$HOME/.local/bin/' to your \$PATH."
+}
+
+if [[ -f $HOME/.local/bin/bashpass ]]; then
+   Update
+else
+   Install
+fi
