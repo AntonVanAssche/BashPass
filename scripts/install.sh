a8de0b3d3378f375406db7eece0859992749b3b4
Anton <vanasscheanton@gmail.com>
2022-11-24 08:47:19 +0100

    Set tabs to 4 spaces

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,34 +12,34 @@ GetLatestRelease() {
 }
 
 Main() {
-   # Detect user
-   if [[ "${UID}" == 0 ]]; then
-      printf 'You must run this as a normal user.\nCurrent user ID: %s %s\n' "${UID}" "$([[ "${UID}" -eq 0 ]] && printf "(root)")"
-      exit 13
-   fi
+    # Detect user
+    if [[ "${UID}" == 0 ]]; then
+        printf 'You must run this as a normal user.\nCurrent user ID: %s %s\n' "${UID}" "$([[ "${UID}" -eq 0 ]] && printf "(root)")"
+        exit 13
+    fi
 
-   printf 'Installing BashPass version: '\''%s'\''...' "${latestVersion}"
-   printf '\n'
+    printf 'Installing BashPass version: '\''%s'\''...' "${latestVersion}"
+    printf '\n'
 
-   git clone https://www.github.com/AntonVanAssche/BashPass.git --branch "$(GetLatestRelease)"
+    git clone https://www.github.com/AntonVanAssche/BashPass.git --branch "$(GetLatestRelease)"
 
-   mkdir -p "${HOME}/.config/bashpass"
-   mkdir -p "${HOME}/.local/share/bashpass"
-   mkdir -p "${HOME}/.local/bin"
+    mkdir -p "${HOME}/.config/bashpass"
+    mkdir -p "${HOME}/.local/share/bashpass"
+    mkdir -p "${HOME}/.local/bin"
 
-   cp -r "${currentDir}/BashPass/config/bashpass.conf" "${HOME}/.config/bashpass/bashpass.conf"
-   cp -r "${currentDir}/BashPass/bashpass" "${HOME}/.local/bin/bashpass"
+    cp -r "${currentDir}/BashPass/config/bashpass.conf" "${HOME}/.config/bashpass/bashpass.conf"
+    cp -r "${currentDir}/BashPass/bashpass" "${HOME}/.local/bin/bashpass"
 
-   printf 'Installing man pages'
-   printf '\n'
+    printf 'Installing man pages'
+    printf '\n'
 
-   [[ ! -d "${HOME}/.local/share/man/man1/" ]] && mkdir -p "${HOME}/.local/share/man/man1/"
-   cp -r "${currentDir}"/BashPass/docs/bashpass.{1,conf.1}.gz "${HOME}/.local/share/man/man1/"
+    [[ ! -d "${HOME}/.local/share/man/man1/" ]] && mkdir -p "${HOME}/.local/share/man/man1/"
+    cp -r "${currentDir}"/BashPass/docs/bashpass.{1,conf.1}.gz "${HOME}/.local/share/man/man1/"
 
-   printf 'Make sure to add '\''\${HOME}/.local/bin/'\'' to your \$PATH.'
-   printf '\n'
+    printf 'Make sure to add '\''\${HOME}/.local/bin/'\'' to your \$PATH.'
+    printf '\n'
 
-   rm -rf BashPass/
+    rm -rf BashPass/
 }
 
 Main "${@}"
--- scripts/update.sh
+++ scripts/update.sh
@@ -17,68 +17,68 @@ cp "${config}" "${oldConfig}"
 # This basically replaces the '$(grep "setting" "$oldConfig" | cut -d: -f2)'.
 # It's also pure bash, which means that no sub shells are used.
 GetOldSetting() {
-   while read -r;
-   do
-      if [[ "${REPLY}" =~ ^${1} ]]; then
-         printf '%s' "${REPLY#*:}"
-         break
-      fi
-   done < "${oldConfig}"
+    while read -r;
+    do
+        if [[ "${REPLY}" =~ ^${1} ]]; then
+            printf '%s' "${REPLY#*:}"
+            break
+        fi
+    done < "${oldConfig}"
 }
 
 # Simple function to get the setting '${1}' from the config file.
 # This basically replaces the '$(grep "setting" "$config" | cut -d" " -f2)'.
 # It's also pure bash, which means that no sub shells are used.
 GetNewSetting() {
-   while read -r;
-   do
-      if [[ "${REPLY}" =~ ^${1} ]]; then
-         printf '%s' "${REPLY#*: }"
-         break
-      fi
-   done < "${config}"
+    while read -r;
+    do
+        if [[ "${REPLY}" =~ ^${1} ]]; then
+            printf '%s' "${REPLY#*: }"
+            break
+        fi
+    done < "${config}"
 }
 
 # Simple function to replace the default settings inside the new config file with your personal settings form the old config file.
 # This basically replaces the 'sed -i "2s|setting|${newSetting}|g" "${config}"'.
 # It's also pure bash, which means that no sub shells are used.
 ReplaceOldSettings() {
-   while read -r;
-   do
-      case ${REPLY} in
-         "location: "*)
-            setting+=("${REPLY%%:*}: ${currentLocation}")
-            ;;
-         "timer: "*)
-            setting+=("${REPLY%%:*}: ${currentTimer}")
-            ;;
-         *)
-            setting+=("${REPLY}")
-            ;;
-      esac
-   done < "${config}"
+    while read -r;
+    do
+        case ${REPLY} in
+            "location: "*)
+                setting+=("${REPLY%%:*}: ${currentLocation}")
+                ;;
+            "timer: "*)
+                setting+=("${REPLY%%:*}: ${currentTimer}")
+                ;;
+            *)
+                setting+=("${REPLY}")
+                ;;
+        esac
+    done < "${config}"
 
-   printf '%s\n' "${setting[@]}" > "${config}"
-   printf 'email: %s\n' "${currentEmail}" >> "${config}"
+    printf '%s\n' "${setting[@]}" > "${config}"
+    printf 'email: %s\n' "${currentEmail}" >> "${config}"
 }
 
 # Simple function to replace the version number inside the config file.
 # This basically replaces the 'sed -i "1s|${currentVersion}|${latestVersion}|g" "${config}"'.
 # It's also pure bash, which means that no sub shells are used.
 ReplaceVersion() {
-   while read -r;
-   do
-      case ${REPLY} in
-         "version: "*)
-            setting+=("${REPLY%%:*}: ${latestVersion}")
-            ;;
-         *)
-            setting+=("${REPLY}")
-            ;;
-      esac
-   done < "${config}"
+    while read -r;
+    do
+        case ${REPLY} in
+            "version: "*)
+                setting+=("${REPLY%%:*}: ${latestVersion}")
+                ;;
+            *)
+                setting+=("${REPLY}")
+                ;;
+        esac
+    done < "${config}"
 
-   printf '%s\n' "${setting[@]}" > "${config}"
+    printf '%s\n' "${setting[@]}" > "${config}"
 }
 
 # Get latest release from GitHub api
@@ -87,43 +87,43 @@ GetLatestRelease() {
 }
 
 Main() {
-   # Detect user
-   if [[ "${UID}" == 0 ]]; then
-      printf 'You must run this as a normal user.\nCurrent user ID: %s %s\n' "${UID}" "$([[ "${UID}" -eq 0 ]] && printf "(root)")"
-      exit 13
-   fi
+    # Detect user
+    if [[ "${UID}" == 0 ]]; then
+        printf 'You must run this as a normal user.\nCurrent user ID: %s %s\n' "${UID}" "$([[ "${UID}" -eq 0 ]] && printf "(root)")"
+        exit 13
+    fi
 
-   printf 'Updating BashPass to version: '\''%s'\''...\n' "${latestVersion}"
-   printf '\n'
+    printf 'Updating BashPass to version: '\''%s'\''...\n' "${latestVersion}"
+    printf '\n'
 
-   git clone https://www.github.com/AntonVanAssche/BashPass.git --branch "$(GetLatestRelease)"
+    git clone https://www.github.com/AntonVanAssche/BashPass.git --branch "$(GetLatestRelease)"
 
-   case $(GetOldSetting 'version') in
-      "1.0"| "1.1")
-         # This fixes a bug where the e-mail address was not copied over to the new config format.
-         printf '\n' >> "${oldConfig}"
+    case $(GetOldSetting 'version') in
+        "1.0"| "1.1")
+            # This fixes a bug where the e-mail address was not copied over to the new config format.
+            printf '\n' >> "${oldConfig}"
 
-         currentEmail="$(GetOldSetting 'email')" || :
-         currentLocation="$(GetOldSetting 'location')" || :
-         currentTimer="$(GetOldSetting 'timer')" || :
+            currentEmail="$(GetOldSetting 'email')" || :
+            currentLocation="$(GetOldSetting 'location')" || :
+            currentTimer="$(GetOldSetting 'timer')" || :
 
-         cp -r "${currentDir}"/BashPass/config/bashpass.conf "${config}"
+            cp -r "${currentDir}"/BashPass/config/bashpass.conf "${config}"
 
-         ReplaceOldSettings
-         ;;
-      *)
-         ReplaceVersion
-         ;;
-   esac
+            ReplaceOldSettings
+            ;;
+        *)
+            ReplaceVersion
+            ;;
+    esac
 
-   printf 'Installing man pages'
-   printf '\n'
+    printf 'Installing man pages'
+    printf '\n'
 
-   [[ ! -d "${HOME}/.local/share/man/man1/" ]] && mkdir -p "${HOME}/.local/share/man/man1/"
-   cp -r "${currentDir}"/BashPass/docs/bashpass.{1,conf.1}.gz "${HOME}/.local/share/man/man1/"
+    [[ ! -d "${HOME}/.local/share/man/man1/" ]] && mkdir -p "${HOME}/.local/share/man/man1/"
+    cp -r "${currentDir}"/BashPass/docs/bashpass.{1,conf.1}.gz "${HOME}/.local/share/man/man1/"
 
-   cp -r "${currentDir}/BashPass/bashpass" "${HOME}/.local/bin/bashpass"
-   rm -rf "${oldConfig}" BashPass/
+    cp -r "${currentDir}/BashPass/bashpass" "${HOME}/.local/bin/bashpass"
+    rm -rf "${oldConfig}" BashPass/
 }
 
 Main "${@}"
