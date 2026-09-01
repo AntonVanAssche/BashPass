c7ac60eac8642c240c30d323bc75289e1b81d9ac
Anton Van Assche <vanasscheanton@gmail.com>
2026-09-01 22:48:26 +0200

    Update Bash completion script
    To align it with the current available options.

--- scripts/bashpass-completion.sh
+++ scripts/bashpass-completion.sh
@@ -4,22 +4,18 @@
 __bashpass_completion() {
     local cur_word="${COMP_WORDS[COMP_CWORD]}"
     local prev_word="${COMP_WORDS[COMP_CWORD-1]}"
-    local options='--help -h --add -a --copy -c --delete -d --show -s --update -u --list -l --sync -S'
-    COMPREPLY=()
+    local options='--help -h --version -v --add -a --copy -c --delete -d --show -s --update -u --list -l --git -G'
 
-    [[ "${cur_word}" == -* ]] && \
-        COMPREPLY+=( $(compgen -W "${options}" -- "${COMP_WORDS[COMP_CWORD]}") )
+    COMPREPLY=()
 
     case "${prev_word}" in
         '-a'|'--add'|'-c'|'--copy'|'-d'|'--delete'|'-s'|'--show'|'-u'|'--update')
             COMPREPLY=(
-                $(compgen -W "$(bashpass --list | tail -n +2)" -- "${COMP_WORDS[COMP_CWORD]}")
+                $(compgen -W "$(bashpass --list | tail -n +2)" -- "${cur_word}")
             )
             ;;
-        'S'|'--sync')
-            COMPREPLY=(
-                $(compgen -W 'download upload' -- "${COMP_WORDS[COMP_CWORD]}")
-            )
+        *)
+            [[ "${cur_word}" == -* ]] && COMPREPLY=( $(compgen -W "${options}" -- "${cur_word}") )
             ;;
     esac
 }
